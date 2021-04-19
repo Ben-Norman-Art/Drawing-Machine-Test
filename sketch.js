3c6ceb83e3796841fc8183cca2da5913dcdb7944
unknown <ben.norman34@gmail.com>
2021-04-19 14:17:29 -0700

    Update on all files

--- sketch.js
+++ sketch.js
@@ -1,75 +1,32 @@
-let input;
-let img;
-let canvas;
-let imgAdd;
-let imgSave;
-let imgReset;
-let reX;
+let myImage;
 let c;
 
+
 function setup() {
-  createCanvas(600, 400);
-  background(220);
-  noStroke();
-  c = addImage.get(0, 360);
-  fill(c);
+   createCanvas(400, 400);
+ background(220);
+
+  strokeWeight(1);
 
   input = createFileInput(handleFile);
   input.parent('upload-button');
 
   imgAdd = createButton('Add Image To Canvas');
   imgAdd.parent('imgAdd-button');
-  imgAdd.mousePressed(addImage);
-
-
+  imgAdd.mousePressed(loadImage);
 }
 
+//get() returns color here
 function draw() {
-
-  //background(myImage);
-  if (mouseIsPressed) {
-    c = addImage.get(mouseY, mouseX);
-    fill(c);
-    rectMode(CENTER);
-    rect(mouseX, mouseY, 10, 10);
+  if (mouseIsPressed){
+  c = myImage.get(mouseX, mouseY);
+  fill(c);
+  rectMode(CENTER);
+  rect(mouseX, mouseY, 10, 10);
   }
 }
 
 
-function handleFile(file) {
-  print(file);
-  if (file.type === 'image') {
-    img = createImg(file.data, '');
-    img.hide();
-  } else {
-    img = null;
-  }
-}
-
-
-//Function for drawing the image onto the canvas.
-function addImage() {
-  reX = (img.width) / (img.height);
-  resizeCanvas(reX * 500, 500);
-  if (img) {
-    image(img, 0, 0, width, height);
-  }
-}
-
-
-function keyTyped() {
-
-  //console.log(`Key s is being Typed`)
-
-  if (key == 's') {
-    saveCanvas(`fileName`, `png`);
-  }
-  if (key == 'c') {
-    clear();
-    background(220);
-  }
-
-
-
-  return false;
+function preload() {
+  myImage = loadImage(addImage);
 }
